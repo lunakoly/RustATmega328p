447fb6f91362aa01724543cb72dfe7652b96e251
Nick Lunyak <lunyak.kolya@mail.ru>
2021-09-06 22:03:04 +0300

    Add size optimizations to the release build

--- build.bat
+++ build.bat
@@ -5,3 +5,5 @@ set AVR_CPU_FREQUENCY_HZ=8000000
 
 cargo build -Z build-std=core --target avr-atmega328p.json --release
 @REM cargo build -Z build-std=core --target avr-atmega328p.json
+
+avr-strip target\avr-atmega328p\release\simple_test.elf
