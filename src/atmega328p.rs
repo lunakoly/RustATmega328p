afcefc73153c8f2edcd41a648ef9c539b372a9f4
Nick Lunyak <lunyak.kolya@mail.ru>
2021-09-28 23:41:44 +0300

    Add more register bindings

--- src/atmega328p.rs
+++ src/atmega328p.rs
@@ -35,3 +35,29 @@ pub const PIND: *mut u8 = 0x29 as *mut u8;
 pub const DDRD: *mut u8 = 0x2a as *mut u8;
 
 pub const PORTD: *mut u8 = 0x2b as *mut u8;
+
+pub const ADMUX: *mut u8 = 0x7c as *mut u8;
+pub const REFS0: u8 = 1 << 6;
+
+pub const ADCSRA: *mut u8 = 0x7a as *mut u8;
+pub const ADEN0: u8 = 1 << 7;
+pub const ADSC0: u8 = 1 << 6;
+pub const ACO0: u8 = 1 << 5;
+pub const ADIF0: u8 = 1 << 4;
+pub const ADIE0: u8 = 1 << 3;
+pub const ADPS2: u8 = 1 << 2;
+pub const ADPS1: u8 = 1 << 1;
+pub const ADPS0: u8 = 1 << 0;
+
+pub const ADCSRB: *mut u8 = 0x7b as *mut u8;
+
+pub const ADC: *mut u16 = 0x78 as *mut u16;
+
+pub const ACSR: *mut u8 = 0x50 as *mut u8;
+pub const ACD0: u8 = 1 << 7;
+
+pub const DIDR0: *mut u8 = 0x7e as *mut u8;
+pub const ADC0D0: u8 = 1 << 0;
+
+pub const PRR: *mut u8 = 0x64 as *mut u8;
+pub const PRADC0: u8 = 1 << 0;
